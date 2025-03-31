ac11a4359ff8e963f67121f48e9a63f3ef942d00
KyleLota <kylelota.16@gmail.com>
2025-03-31 09:32:58 +0800

    Revised code

--- HelloSir.java
+++ HelloSir.java
@@ -1,5 +1,33 @@
-public class HelloSir{
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+public class HelloSirMadz {
+
     public static void main(String[] args) {
-        System.out.println("Hello, Sir Madz!");
+        displayGreeting();
+        displayRandomNumber();
+        displayCurrentTime();
+    }
+
+    public static void displayGreeting() {
+        String[] greetings = {"Hello Sir Madz!", "Greetings Sir Madz!", "Salutations Sir Madz!", "Welcome Sir Madz!", "Ahoy Sir Madz!"};
+        Random random = new Random();
+        int randomIndex = random.nextInt(greetings.length);
+        System.out.println(greetings[randomIndex]);
+    }
+
+    public static void displayRandomNumber() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(100); 
+        System.out.println("A random number for Sir Madz: " + randomNumber);
+    }
+
+    public static void displayCurrentTime() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = now.format(formatter);
+        System.out.println("Current time for Sir Madz: " + formattedDateTime);
     }
 }
+
